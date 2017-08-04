5300a67297004c3ca74864f5dbd1d1c557198497
Nicholas Hoover <n.hoover13@gmail.com>
2017-08-03 22:43:17 -0700

    made a json object that holds an array or arrays of data

--- index.php
+++ index.php
@@ -15,16 +15,27 @@ and open the template in the editor.
         $file = filter_input(INPUT_GET, 'request');
         $offset = filter_input(INPUT_GET, 'offset');
         $limit = filter_input(INPUT_GET, 'limit');
-        $row = 0;
+        $row = 1;
+        $rArray = [];
+        $myJSON;
         
         if (($handle = fopen("data/$file.csv", "r")) !== FALSE) {
+            
+            $data = fgetcsv($handle, 100, ",");
+            $columns = $data;
+            
+                 
             while (($data = fgetcsv($handle, 100, ",")) !== FALSE) {
                 if($row >= $offset && $row < ($limit + $offset)){
                     $num = count($data);
+                    $xArray;
                     for ($c=0; $c < $num; $c++) {
+                        $xArray[$columns[$c]] = $data[$c];
                         
                         echo $data[$c] . "<br />\n";
                     }
+                    array_push($rArray,$xArray);
+                    
                 
                     
                     
@@ -33,7 +44,8 @@ and open the template in the editor.
             }
             fclose($handle);
         }
-        
+        $myJSON = json_encode($rArray);
+        echo $myJSON;
 
         
         ?>
