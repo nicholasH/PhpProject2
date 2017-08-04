32664d9418032e120a2d0eb6571e254c2c2bf428
nicholasH <nicholas.hoover@gmail.com>
2017-08-03 21:25:18 -0700

    Got the data from $_get

--- index.php
+++ index.php
@@ -11,22 +11,30 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            
+        $file = filter_input(INPUT_GET, 'request');
+        $offset = filter_input(INPUT_GET, 'offset');
+        $limit = filter_input(INPUT_GET, 'limit');
+        $row = 0;
+        
+        if (($handle = fopen("data/$file.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 100, ",")) !== FALSE) {
+                if($row >= $offset && $row < ($limit + $offset)){
+                    $num = count($data);
+                    for ($c=0; $c < $num; $c++) {
+                        
+                        echo $data[$c] . "<br />\n";
+                    }
                 
-        foreach ($_REQUEST as $x)
-        {
-            echo  $x ;
-            echo "<br>";
+                    
+                    
+                }
+            $row++;
+            }
+            fclose($handle);
         }
+        
 
-
-        $data = $_REQUEST["request"];
-        $offest = $_REQUEST["offset"];
-        $limit = $_REQUEST['limit'];
-        echo '<br>test';
-        echo $data;
-        echo '<br>test';
-        echo $offest;
-        echo $limit;
         
         ?>
     </body>
