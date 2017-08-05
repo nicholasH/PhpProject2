76d1cbf77890351eba599aef897fb1fd0687c9b6
Nicholas Hoover <n.hoover13@gmail.com>
2017-08-05 00:38:25 -0700

    added comments and fixed error checking

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
-
+//gets the inputs from the url 
 $file = filter_input(INPUT_GET, 'request');
 $offset = filter_input(INPUT_GET, 'offset');
 $limit = filter_input(INPUT_GET, 'limit');
+
+if(!is_numeric($offset) or ! is_numeric($limit)){
+    $offset = 0;
+    $limit = 0;
+}
+
 $row = 1;
+//the return array that will hold the all the wanted data
 $rArray = [];
 $myJSON;
 
+
+//open files from where the data is stored 
 if (($handle = fopen("data/$file.csv", "r")) !== FALSE) {
+    //gets the columns names from the first line on the cvs
+    $columns = fgetcsv($handle, 100, ",");
 
-    $data = fgetcsv($handle, 100, ",");
-    $columns = $data;
-
-
+    //loops through the data
     while (($data = fgetcsv($handle, 100, ",")) !== FALSE) {
         if ($row >= $offset && $row < ($limit + $offset)) {
             $num = count($data);
@@ -27,6 +35,7 @@ if (($handle = fopen("data/$file.csv", "r")) !== FALSE) {
     fclose($handle);
 }
 $myJSON = json_encode($rArray);
+//return the data
 echo $myJSON;
 return $myJSON;
 
